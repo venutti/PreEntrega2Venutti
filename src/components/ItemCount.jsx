493d993a8cacc424b9738200aef84dd5caffe9ff
venutti <lvenutti@fi.uba.ar>
2023-03-13 21:35:58 -0300

    Agrega ItemCount component

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,59 @@
+import { Box, Button, ButtonGroup } from "@mui/material";
+import { useState } from "react";
+
+const ItemCount = ({ stock, onAdd }) => {
+  const [count, setCount] = useState(1);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    if (count === 1) return;
+    setCount(count - 1);
+  };
+
+  const handleAdd = () => {
+    if (count > stock) return;
+    onAdd(count);
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "10px",
+      }}
+    >
+      <ButtonGroup color="warning" variant="contained" disableElevation>
+        <Button onClick={decrement}>-</Button>
+        <Box
+          sx={{
+            padding: "5px 30px",
+            display: "grid",
+            placeContent: "center",
+            borderBlockStart: "1px solid",
+            borderBlockEnd: "1px solid",
+            borderColor: "warning.dark",
+          }}
+        >
+          {count}
+        </Box>
+        <Button onClick={increment}>+</Button>
+      </ButtonGroup>
+
+      <Button
+        variant="contained"
+        color="success"
+        disableElevation
+        onClick={handleAdd}
+      >
+        Agregar al carrito
+      </Button>
+    </Box>
+  );
+};
+
+export default ItemCount;
