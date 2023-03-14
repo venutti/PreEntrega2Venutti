783b238ecff20e44c3860f6a9c6ed939c558ab12
venutti <lvenutti@fi.uba.ar>
2023-03-13 21:36:22 -0300

    Agrega Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import Navbar from "./components/Navbar";
 import ItemListContainer from "./components/ItemListContainer";
+import { Route, Routes } from "react-router-dom";
+import ItemCount from "./components/ItemCount";
 
 const App = () => {
   return (
     <>
       <CssBaseline />
       <Navbar />
-      <ItemListContainer greeting="Bienvenido a BoogieShop. Una tienda de accesorios para Halloween! Próximamente vendran más productos..." />
+      <ItemCount stock={3} onAdd={(count) => console.log(count)} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ItemListContainer greeting="Bienvenido a BoogieShop. Una tienda de accesorios para Halloween! Próximamente vendran más productos..." />
+          }
+        />
+        <Route
+          path="/category/:id"
+          element={<p>Contenedor de lista filtrada por categoría</p>}
+        />
+        <Route
+          path="/item/:id"
+          element={<p>Contenedor del detalle del producto</p>}
+        />
+      </Routes>
     </>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,13 @@ import Typography from "@mui/material/Typography";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Button from "@mui/material/Button";
 import CartWidget from "./CartWidget";
+import { Link } from "react-router-dom";
+
+const links = [
+  { route: "/category/clothes", label: "Ropa" },
+  { route: "/category/shoes", label: "Calzado" },
+  { route: "/category/deco", label: "Decoración" },
+];
 
 const Navbar = () => {
   return (
@@ -12,12 +19,14 @@ const Navbar = () => {
       alignItems="center"
       sx={{ backgroundColor: "warning.light", p: 2 }}
     >
-      <Typography
-        variant="h4"
-        sx={{ fontWeight: "bold", letterSpacing: -2, mr: 4 }}
-      >
-        BoogieShop
-      </Typography>
+      <Link to="/">
+        <Typography
+          variant="h4"
+          sx={{ fontWeight: "bold", letterSpacing: -2, mr: 4 }}
+        >
+          BoogieShop
+        </Typography>
+      </Link>
       <ButtonGroup color="inherit" variant="text" sx={{ mr: "auto" }}>
         <Button>Inicio</Button>
         <Button>Productos</Button>
