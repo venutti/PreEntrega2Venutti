bbaa22bbe56547f3b601f0b37b3982ab4f98051e
venutti <lvenutti@fi.uba.ar>
2023-03-22 14:27:17 -0300

    CartWidget consume context
    Agrega la funcionalidad de que cartWidget sepa la cantidad de elementos que hay en el carrito

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,11 +1,19 @@
 import IconButton from "@mui/material/IconButton";
 import Badge from "@mui/material/Badge";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { useContext } from "react";
+import { CartContext } from "../contexts/CartContext";
 
 const CartWidget = () => {
+  const { cartList } = useContext(CartContext);
+  const quantityInCart = cartList.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
+
   return (
     <IconButton>
-      <Badge badgeContent={4} color="error">
+      <Badge badgeContent={quantityInCart} color="error">
         <ShoppingCartOutlinedIcon sx={{ color: "black" }} />
       </Badge>
     </IconButton>
