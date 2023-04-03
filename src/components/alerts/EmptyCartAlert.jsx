294b2e90bfbf3630f7467c813babbe9d89ad6369
venutti <lvenutti@fi.uba.ar>
2023-04-03 18:29:01 -0300

    Refactor de alerts
    Crea un componente Alert que es común a todas las alertas

--- src/components/alerts/EmptyCartAlert.jsx
+++ src/components/alerts/EmptyCartAlert.jsx
@@ -1,23 +1,13 @@
-import { Button, Container } from "@mui/material";
-import Subtitle from "../shared/Subtitle";
-import { Link } from "react-router-dom";
 import emptyGhostImage from "./assets/empty_ghost.png";
+import Alert from "./Alert";
 
 const EmptyCartAlert = () => {
   return (
-    <Container
-      maxWidth="sm"
-      sx={{ my: 2, display: "grid", justifyItems: "center", gap: 1 }}
-    >
-      <img style={{ maxWidth: "100%", height: "auto" }} src={emptyGhostImage} />
-      <Subtitle>
-        <strong>¡Ups!</strong>
-        <br /> Parece que tu carrito está vacío
-      </Subtitle>
-      <Button component={Link} to="/" variant="contained" color="primary">
-        Volver al listado
-      </Button>
-    </Container>
+    <Alert
+      image={emptyGhostImage}
+      title={"¡Ups!"}
+      subtitle={"Parece que tu carrito está vacío."}
+    />
   );
 };
 
--- src/components/alerts/NotFoudAlert.jsx
+++ src/components/alerts/NotFoudAlert.jsx
@@ -1,27 +1,15 @@
-import { Button, Container } from "@mui/material";
-import Subtitle from "../shared/Subtitle";
-import { Link } from "react-router-dom";
 import surpriseGhostImage from "./assets/surprise_ghost.png";
+import Alert from "./Alert";
 
 const NotFoundAlert = () => {
   return (
-    <Container
-      maxWidth="sm"
-      sx={{ my: 2, display: "grid", justifyItems: "center", gap: 1 }}
-    >
-      <img
-        style={{ maxWidth: "90%", height: "auto" }}
-        src={surpriseGhostImage}
-      />
-      <Subtitle>
-        <strong>¡Oops! Página no encontrada</strong>
-        <br /> Parece que este fantasma ha asustado a nuestra página y la ha
-        hecho desaparecer.
-      </Subtitle>
-      <Button component={Link} to="/" variant="contained" color="primary">
-        Volver al listado
-      </Button>
-    </Container>
+    <Alert
+      image={surpriseGhostImage}
+      title={"¡Oops! Página no encontrada"}
+      subtitle={
+        "Parece que este fantasma ha asustado a nuestra página y la ha hecho desaparecer."
+      }
+    />
   );
 };
 
